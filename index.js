ae8fa4ce84afcc1245441a93fd0cb8514975fcd5
John Clever <jclever114@gmail.com>
2020-05-30 11:07:19 +0100

    third commit

--- index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-    var navbar = document.querySelector(".navbar");
-    var about = document.querySelector(".about");
-    navbar.classList.toggle("active", window.scrollY > 0);
-    about.classList.toggle("sticky", window.scrollY > 0);
-
-})
